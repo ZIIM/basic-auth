c5a812a8cb1346de18864d252d742eea8ec70efe
JOHN T GAUSE III <johntgause12@gmail.com>
2024-04-11 23:28:12 -0700

    user model

---
+++ src/auth/models/users-model.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+
+const User = (Sequelize, DataTypes) => {
+  return Sequelize.define("User", {
+    username:{
+		type: DataTypes.STRING,
+		// meaning allowNull means fields cant be empty
+        allowNull:false,
+        },
+	password:{
+		type: DataTypes.STRING,
+		allowNull:false,
+	},
+    });
+};
+
+module.exports = User;
